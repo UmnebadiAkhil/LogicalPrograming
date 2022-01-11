239efaa83c4f8fdae34b0eaa137e4431511b1acd
Akhil <akhilu5042@gmail.com>
2022-01-11 17:27:31 +0530

    StopWatch

---
+++ StopWatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograming
+{
+    internal class StopWatch
+    {
+        public void Calculate()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(6);
+            }
+            sw.Stop();
+            Console.WriteLine("The lepse time is:{0:hh\\:mm\\:ss}", sw.Elapsed);
+        }
+    }
+}
