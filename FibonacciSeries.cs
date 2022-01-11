f3a2a04c7f1fcf55e4161aa93fa42edb835ab971
Akhil <akhilu5042@gmail.com>
2022-01-11 17:19:56 +0530

    FibonacciSeries.cs

---
+++ FibonacciSeries.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograming
+{
+    internal class FibonacciSeries
+    {
+        public void Series()
+        {
+            var fibonacciNumbers = new List<long> { 1, 1 };
+            Console.Write("Please enter the nth Numbers : ");
+            long n = Convert.ToInt64(Console.ReadLine());
+            while (fibonacciNumbers.Count < n)
+            {
+                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+                fibonacciNumbers.Add(previous + previous2);
+
+            }
+            foreach (var item in fibonacciNumbers)
+                Console.WriteLine(item);
+        }
+    }
+}
